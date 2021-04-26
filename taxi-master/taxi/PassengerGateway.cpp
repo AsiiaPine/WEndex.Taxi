91ef1f84a0ca8fe97fd1bcf6a5256982befe534b
AsiiaPine <70723894+AsiiaPine@users.noreply.github.com>
2021-04-26 23:54:14 +0300

    Update PassengerGateway.cpp

--- taxi-master/taxi/PassengerGateway.cpp
+++ taxi-master/taxi/PassengerGateway.cpp
@@ -2,62 +2,114 @@
 // Created by asiia on 01.04.2021.
 //
 
-#ifndef WENDEX_TAXI_PASSENGERGATEWAY_H
-#define WENDEX_TAXI_PASSENGERGATEWAY_H
-
-
-#include "UserAppData/paymentMethods.h"
-#include "Address.h"
-#include "CarType.h"
-#include "UserAppData/paymentMethods.h"
-#include "UserAppData/Passenger.h"
-#include "Order.h"
-#include "WendexTaxi.h"
+#include <iostream>
+#include "PassengerGateway.h"
+#include "Repository.h"
 #include "AccessManager.h"
 
-class PassengerGateway {
-private:
-
-    AccessManager *accessManager;
-    UserModel *identity;
-    struct System *system;
-    Passenger* passenger;
-    Order* currentOrder;
-    //static OrderRepository* orderRepository;
-    static vector<Passenger*> AllPassengers;
-
-    UserRepository *userRepository;
-    OrderRepository *orderRepository;
-    AddressRepository *addressRepository;
-    PassengerRepository *passengerRepository;
-    DriverRepository *driverRepository;
-    CarRepository *carRepository;
-
-public:
-
-    PassengerGateway(System *system, UserModel *identity, AccessManager *accessManager);
+std::vector<Passenger *> PassengerGateway::AllPassengers;
 
 
-    virtual vector<Order *> SeeOrderHistory() ;
+PassengerGateway::PassengerGateway(System *system, UserModel *identity, AccessManager *accessManager)
+        : system(system), identity(identity), accessManager(accessManager), userRepository(system->userRepository),
+          orderRepository(system->orderRepository), addressRepository(system->addressRepository),
+          driverRepository(system->driverRepository), passengerRepository(system->passengerRepository) {
+}
 
-    virtual PaymentMethod SeePaymentMethod();
+//PassengerGateway::PassengerGateway(Passenger *passenger) : passenger(passenger), currentOrder(NULL) {}
 
-    virtual void UpdatePaymentMethod(PaymentMethod newMethod) ;
+//PassengerGateway* PassengerGateway::SignUp(string UserName, string password, string email) {
+//    if(password.size()<8){
+//        cout<<"Length of password is less than 8 symbols"<<endl;
+//        return nullptr;
+//    }
+//    auto passenger=new Passenger(UserName,password,email);
+//    auto gate=new PassengerGateway(passenger);
+//    AllPassengers.push_back(passenger);
+//}
 
-    virtual Address SeePinnedAddress();
+//void PassengerGateway::LogIn(string username, string password){
+//    for(Passenger* aPassenger: PassengerGateway::AllPassengers){
+//        if((aPassenger->name == username) & (aPassenger->password == password)){
+//            std::cout << "Welcome!" << aPassenger->name << endl;
+//            PassengerGateway* gateway= new PassengerGateway(aPassenger);
+//            return;
+//        }
+//    }
+//    cout<<"Wrong user name or password"<<endl;
+//}
 
-    virtual void UpdateAddress(Address adr);
+vector<Order *> PassengerGateway::SeeOrderHistory() {
+    auto orderModels = this->orderRepository->FindAllByPassengerId(this->identity->id);
+    auto orders = std::vector<Order *>();
+    for (auto model : orderModels) {
+        // endAddress repo
+        auto a1 = addressRepository->FindEntityById(model->startAddressId);
+        auto a2 = addressRepository->FindEntityById(model->endAddressId);
+        auto pM = passengerRepository->FindPassengerByUserId(model->passengerId);
+        auto upM = userRepository->FindEntityById(model->passengerId);
+        auto p = new Passenger(upM,pM);
 
-    virtual void
-    OrderTheRide(CarType type, Address startAddress, Address destinationAddress, int cost, PaymentMethod paymentMethod);
+        auto dM = driverRepository->FindDriverByUserId(model->driverId);
+        auto udM = userRepository->FindEntityById(model->driverId);
+        //TODO: extract
+        auto car = new Car(carRepository->FindEntityById(dM->car));
+        auto d = new Driver(udM, dM, car);
+        //
+        orders.push_back(new Order(model,*a1,*a2,PaymentMethod::Card,p,d));
+        // TODO: transform to dto
+    }
+    return orders;
+}
 
-    virtual Coords CurrentCoordinates();
+void PassengerGateway::OrderTheRide(
+        CarType type,
+        Address startAddress,
+        Address destinationAddress,
+        int cost,
+        PaymentMethod paymentMethod
+) {
+    if (accessManager->requireAccess(this->identity, Action::ORDER_RIDE)) {
+        // TODO: create order
+        auto order = OrderModel{.passengerId=identity->id,.driverId = 0, .cost = cost,
+                                .startAddressId=startAddress.id,.endAddressId=destinationAddress.id,
+                                .startTime=0, .endTime=0, };
+        //auto order = new Order(this->passenger, startAddress, destinationAddress, cost, paymentMethod, Driver());
+        this->orderRepository->Create(&order);
+    }
+}
 
-    virtual Order *TheBill(Order *order);
+void PassengerGateway::UpdateAddress(Address address) {
+    if (this->currentOrder) {
+        this->currentOrder->setEndAddress(address);
+    }
+}
 
-};
+PaymentMethod PassengerGateway::SeePaymentMethod() {
+    return this->passenger->getPaymentMethod();
+}
 
+Coords PassengerGateway::CurrentCoordinates() {
+    if (this->currentOrder) {
+        //TODO: get coords of a currentCar
+        auto car = this->currentOrder->getDriver()->getCurrentCar();
+        return Coords(car->getCoordX(),car->getCoordY());
+    } else {
+        //TODO: get coords if not on currentCar
+        return Coords(0.0, 0.0);
+    }
+}
 
-PassengerGateway *GetDefaultPassengerGateway(UserModel *identity);
+void PassengerGateway::UpdatePaymentMethod(PaymentMethod newMethod) {
+    if (this->accessManager->requireAccess(this->identity, Action::UPDATE_PAYMENT_METHOD)) {
+        this->currentOrder->setPaymentMethod(newMethod);
+    }
+}
 
-#endif //WENDEX_TAXI_PASSENGERGATEWAY_H
+Order *PassengerGateway::TheBill(Order *order) {
+    return order;
+}
+
+Address PassengerGateway::SeePinnedAddress() {
+    return Address();
+}
