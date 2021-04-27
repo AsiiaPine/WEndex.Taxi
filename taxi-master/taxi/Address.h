517f8b23fdfda87ec1554698f3714c2e9dbe27e8
AsiiaPine <70723894+AsiiaPine@users.noreply.github.com>
2021-04-27 11:51:27 +0300

    Update Address.h

--- taxi-master/taxi/Address.h
+++ taxi-master/taxi/Address.h
@@ -6,25 +6,43 @@
 #define WENDEX_TAXI_ADDRESS_H
 #include <filesystem>
 #include <fcntl.h>
-//
-// Created by asiia on 10.04.2021.
-//
+#include "Repository.h"
+
 #include <string>
 struct Coords{
-    double x;
-    double y;
+    double x = 0;
+    double y = 0;
+
+    Coords() {}
+
+    Coords(double x, double y) : x(x), y(y) {}
 };
 
 using namespace std;
 struct Address{
+    int32_t id;
     Coords coordinate;
     char city[32];
-    string street;
-    string numberOfBuilding;
+    char street[32];
+    char numberOfBuilding[32];
+
+    explicit Address(char *a) {
+        std::strcpy(street,a);
+        coordinate = Coords( *((double*)a+1),*((double*)a+2) );
+    }
 };
-void PrintTheAddress(Address address){
-    cout<<"City: "<<address.city<<"\n Street: "<<address.street<<"\n Number of building: "<<address.numberOfBuilding<<endl;
-}
+
+class AddressRepository : public Repository<Address,sizeof(Address)> {
+public:
+    explicit AddressRepository(Database *db) : Repository(db,db->OpenTable("endAddress.table")) {};
+
+//    Address *FindPassengerByUserId(uint32_t addressId) {
+//        return FindEntityByFieldValue(&userId,offsetof(PassengerModel, userId), sizeof(int32_t));
+//    }
+};
+//void PrintTheAddress(Address endAddress){
+//    cout<<"City: "<<endAddress.city<<"\n Street: "<<endAddress.street<<"\n Number of building: "<<endAddress.numberOfBuilding<<endl;
+//}
 //FILE Map=open(Map.txt,)
 
 
