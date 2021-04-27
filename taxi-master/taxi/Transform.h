099186d1b370c1e2424c7f0802f134111103ce8d
AsiiaPine <70723894+AsiiaPine@users.noreply.github.com>
2021-04-27 12:02:44 +0300

    Create Transform.h

---
+++ taxi-master/taxi/Transform.h
@@ -0,0 +1,10 @@
+
+
+#ifndef WENDEX_TAXI_TRANSFORM_H
+#define WENDEX_TAXI_TRANSFORM_H
+
+#include "WendexTaxi.h"
+
+std::vector<Order*> orderFromModel(System *sys,std::vector<OrderModel *> orderModels);
+
+#endif //WENDEX_TAXI_TRANSFORM_H
