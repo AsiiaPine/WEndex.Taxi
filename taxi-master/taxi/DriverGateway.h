8a6f1f60ff5709494f924add90731988dff16e5f
AsiiaPine <70723894+AsiiaPine@users.noreply.github.com>
2021-04-27 11:53:25 +0300

    Update DriverGateway.h

--- taxi-master/taxi/DriverGateway.h
+++ taxi-master/taxi/DriverGateway.h
@@ -4,16 +4,51 @@
 
 #ifndef WENDEX_TAXI_DRIVERGATEWAY_H
 #define WENDEX_TAXI_DRIVERGATEWAY_H
+
 #include "UserAppData/Driver.h"
 #include "Order.h"
+#include "AccessManager.h"
+#include "WendexTaxi.h"
+
 class DriverGateway {
-    Driver* driver;
 //    IsRiding isRiding;
     double rating;
-    void AcceptOrder(Order* order);
-//    void ChoseStatus(Status newStatus);
-    void IamRiding();
+private:
+    AccessManager *accessManager;
+    UserModel *identity;
+
+    DriverRepository *driverRepository;
+    OrderRepository *orderRepository;
+    CarRepository *carRepository;
+public:
+    struct System *system;
+    Driver *driver;
+    DriverGateway(System *system, UserModel *identity, AccessManager *accessManager);
+
+//    DriverGateway(System *pSystem, UserModel *pModel);
+
+public:
+
+    double GetRating();
+
+    Car GetCurrentCar();
+
+    std::vector<Car> GetAllCars();
+
+    void AcceptOrder(Order *order);
+
+    void DeclineOrder(Order *order);
+
+    int Status();
+
+    void SetStatus(int newStatus);
+
+    std::vector<Order*> GetOrderHistory();
+
+    std::vector<Order*> GetAvailableOrders();
+
     void StopTheRide();
+
 };
 
 
